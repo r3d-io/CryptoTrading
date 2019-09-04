82f9362c8efecacff52520f9b2aa6f294ba2f209
Anuj singh <askoli@techracers.io>
2019-09-04 13:04:30 +0530

    Updated search bar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
 import CryptoContainer from './components/cryptoContainer'
 
 function App() {
