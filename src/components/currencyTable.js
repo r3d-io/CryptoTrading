cc4b2021b34e7ae22d68cc89fa42a875227d8375
Anuj singh <askoli@techracers.io>
2019-09-04 16:35:15 +0530

    Added No currency match check

--- src/components/currencyTable.js
+++ src/components/currencyTable.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 class CurrencyTable extends React.Component {
 
   createTable = () => {
+
     let table = []
-    this.props.coinList.map((values, index) => {
+    if (this.props.coinList.length > 0) {
+      this.props.coinList.map((coin, index) => {
+        console.log("currencyTable", coin)
+        return table.push(
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{coin.currencyCode}</td>
+            <td>{coin.currencyName}</td>
+            <td>{coin.value}</td>
+
+          </tr>)
+      })
+    }
+    else {
       table.push(
-        <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{values.currencyCode}</td>
-          <td>{values.currencyName}</td>
+        <tr key='0'>
+          <td>
+            No matching Currency
+          </td>
         </tr>)
-    })
+    }
     return table
   }
 
