73033f07f56e7d8a21320d1d53bb396714ab7d73
Anuj singh <askoli@techracers.io>
2019-09-04 16:34:38 +0530

    Added loading while price fetch

--- src/components/cryptoContainer.js
+++ src/components/cryptoContainer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
 import SearchCrypto from './searchBar';
 import CurrencyTable from './currencyTable';
 import { connect } from 'react-redux';
-class CryptoContainer extends React.Component {
+import Loading from './loader.js'
+class CryptoContainer extends Component {
   render() {
+    let list;
+    console.log('cryptoContainer',this.props.isLoading)
+    if (this.props.isLoading)
+      list = <Loading />
+    else
+      list = <CurrencyTable coinList={this.props.coinList} />
     return (
       <div>
         <SearchCrypto />
-        <CurrencyTable coinList={this.props.coinList}/>
+        {list}
       </div>
     );
   }
@@ -15,7 +22,8 @@ class CryptoContainer extends React.Component {
 
 const mapStateToProps = (state) => (
   {
-    coinList: state.cryptoList.coinList
+    coinList: state.cryptoList.coinList,
+    isLoading: state.cryptoList.isLoading
   })
 
 export default connect(mapStateToProps)(CryptoContainer);
