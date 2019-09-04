62ae0cf3386aad12681cdbd3daee43edc836052a
Anuj singh <askoli@techracers.io>
2019-09-04 16:36:11 +0530

    Added value parameter

--- src/data/matchCurrency.js
+++ src/data/matchCurrency.js
@@ -1,10 +1,12 @@
 var currencyList = require('./currency.json')
 const filterList = (searchStr) => {
   let matchList = []
-  let regex = RegExp(`^${searchStr}`,'i')
+  let regex = RegExp(`^${searchStr}`, 'i')
   for (let coinIndex in currencyList) {
-    if (regex.test(currencyList[coinIndex].currencyCode) || regex.test(currencyList[coinIndex].currencyName))
+    if (regex.test(currencyList[coinIndex].currencyCode) || regex.test(currencyList[coinIndex].currencyName)) {
+      currencyList[coinIndex].value = 'Not Available'
       matchList.push(currencyList[coinIndex])
+    }
   }
   // console.log(`matchList ${JSON.stringify(matchList)}`)
   return matchList
